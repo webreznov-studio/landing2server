47b5fbd886e5d02c5177bd79b322f61770cd5d2c
webreznov-studio <73492632+webreznov-studio@users.noreply.github.com>
2021-06-10 14:49:14 +0300

    update user form

--- api/email.js
+++ api/email.js
@@ -1,7 +1,6 @@
 const PORT = process.env.PORT || 8080;
 const fs = require('fs');
-const querystring = require('querystring');
-const url = require('url');
+const { nanoid } = require('nanoid')
 
 // Data
 
@@ -11,23 +10,28 @@ fs.readFile('database/user_contact.json', 'utf8', (err, data) => {
     userContact = JSON.parse(data);
 });
 
+const getInfoData = () => {
+    const date = Date.now()
+    return {
+        id: nanoid(8),
+        createDate: date,
+        isNew: true,
+        isRead: false,
+        description: ''
+    }
+}
 
 /********** Api ***********/
 
 /** POST /api/email/add-user-contact */
 console.log(`##email http://localhost:${PORT}/api/email/add-user-contact`);
 exports.toAddUserContact = (req, res) => {
-
-    // console.log('!!!!!!!!111', url.parse(req.url, true).query);
-    console.log('!!!!!!!!222', querystring.parse('w=%D6%D0%CE%C4&foo=bar', null, null, { decodeURIComponent: decodeURIComponent }));
-    console.log('!!!!!!!!333', querystring.parse(req.url, null, null, { decodeURIComponent: decodeURIComponent }));
-
     const mapUserData = {
         name: req.query.name,
         email: req.query.email,
         phone: req.query.phone,
     }
-    userContact.push(mapUserData);
+    userContact.push({...getInfoData(), ...mapUserData});
     var json = JSON.stringify(userContact);
 
     fs.writeFile('database/user_contact.json', json, 'utf8', (err) => {
